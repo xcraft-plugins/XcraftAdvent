aec6c431142ee0f8a3c343680bf0712060ab9e65
Maximilian Stiede <maximilian.stiede@student.hpi.uni-potsdam.de>
2018-12-27 20:05:06 +0100

    using java.time.* instead of Calendar

--- src/main/java/de/groovybyte/spigot/xcraftadvent/DefaultDateChecker.java
+++ src/main/java/de/groovybyte/spigot/xcraftadvent/DefaultDateChecker.java
@@ -1,34 +1,35 @@
 package de.groovybyte.spigot.xcraftadvent;
 
 import com.google.common.collect.Range;
-import java.util.Calendar;
-import java.util.function.Supplier;
+import java.time.Clock;
+import java.time.Month;
+import java.time.MonthDay;
 
 public class DefaultDateChecker implements IDateChecker {
 
-    private final Supplier<Calendar> calendarSupplier;
+    private final Clock clock;
     private final Range<Integer> calendarCreationDates = Range.closed(1, 24);
     private final Range<Integer> calendarOpeningDates = Range.closed(1, 31);
 
     public DefaultDateChecker() {
-        this(Calendar::getInstance);
+        this(Clock.systemDefaultZone());
     }
 
-    public DefaultDateChecker(Supplier<Calendar> calendarSupplier) {
-        this.calendarSupplier = calendarSupplier;
+    public DefaultDateChecker(Clock clock) {
+        this.clock = clock;
     }
-
-    private Calendar getCalendar() {
-        return calendarSupplier.get();
+    
+    private MonthDay currentMonthDay() {
+        return MonthDay.now(clock);
     }
 
     private boolean isDecember() {
-        return getCalendar().get(Calendar.MONTH) == Calendar.DECEMBER;
+        return currentMonthDay().getMonth().equals(Month.DECEMBER);
     }
 
     @Override
     public int getDayOfMonth() {
-        return getCalendar().get(Calendar.DAY_OF_MONTH);
+        return currentMonthDay().getDayOfMonth();
     }
 
     @Override
