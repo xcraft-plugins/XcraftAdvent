ef4ee9c50b94c853f395970be98e66751dcf9175
Maximilian Stiede <maximilian.stiede@student.hpi.de>
2019-01-07 16:38:00 +0100

    outsource permission constants

---
+++ src/main/java/de/groovybyte/spigot/xcraftadvent/Permissions.java
@@ -0,0 +1,11 @@
+package de.groovybyte.spigot.xcraftadvent;
+
+public class Permissions {
+
+	/* fix: give item only in gm 0 */
+	//public final String PERM_admin_peek = "calendar.admin.showAny";
+	public final static String OWN_CALENDAR = "calendar.own";
+	public final static String SHOW_CALENDAR = "calendar.show";
+	public final static String OPEN_CALENDAR_DOORS = "calendar.doors.open";
+
+}
