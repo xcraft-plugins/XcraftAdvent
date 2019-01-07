e26df9b794d45c2a0a1b27f1d5c164ff72447a84
Maximilian Stiede <maximilian.stiede@student.hpi.de>
2019-01-07 16:37:26 +0100

    moving message sending to Messages class

--- src/main/java/de/groovybyte/spigot/xcraftadvent/Messages.java
+++ src/main/java/de/groovybyte/spigot/xcraftadvent/Messages.java
@@ -1,26 +1,49 @@
 package de.groovybyte.spigot.xcraftadvent;
 
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
 
 public final class Messages {
 
-    public final static String CHAT_PREFIX = ChatColor.WHITE + "[" + ChatColor.DARK_GREEN + "Adventskalender" + ChatColor.WHITE + "]" + ChatColor.DARK_AQUA + " ";
-    public final static String INV_TITLE = "Adventskalender";
-    public final static String INV_FILL = "Türchenfüllen";
-    public final static String ITEM_NAME = "Tag %s";
-    public final static String ITEM_EDIT_DAY = "Türchen %s";
-    public final static String ITEM_OPENED = "Bereits geöffnet";
-    public final static String ITEM_CLOSED = "Klicke zum Öffnen";
-    public final static String ITEM_FUTURE = "Überraschung in %s Tag%s";
-    public final static String ITEM_24 = "Weihnachten in %s Tag%s";
-    public final static String ITEM_24_READY = "Frohe Weihnachten!";
-    public final static String MSG_OPEN = "Du hast das Türchen Nummer %d bereits geöffnet!";
-    public final static String MSG_CLOSED = "Du darfst das Türchen Nummer %d noch nicht öffnen!";
-    public final static String MSG_OPENING = "Du hast Türchen Nummer %d geöffnet!";
-    public final static String MSG_OVERFLOW = "Dein Inventar ist voll. Der Türcheninhalt hat sich um dich herum verteilt.";
-    public final static String MSG_MISSING_PERM = "Du darfst das leider nicht tun! (Versuche es nochmal in der Hauptwelt)";
-    public final static String MSG_MISSING_CONTENT = "Die faulen Admins haben das Türchen noch nicht gefüllt ;)";
-    public final static String MSG_WRONG_TIME = "Du kannst den Adventskalender nicht mehr öffnen.";
-    public final static String MSG_WRONG_GAMEMODE = "Du solltest die Türchen nur im Survival-Modus öffnen.";
-    public final static String MSG_COMMAND_INFO = "Du kannst den Adventskalender mit /advent öffnen.";
+	public final static String CHAT_PREFIX = ChatColor.WHITE + "[" + ChatColor.DARK_GREEN + "Adventskalender" + ChatColor.WHITE + "]" + ChatColor.DARK_AQUA + " ";
+	private final static BaseComponent[] PREFIX = TextComponent.fromLegacyText(CHAT_PREFIX);
+	public final static String INV_TITLE = "Adventskalender";
+	public final static String INV_FILL = "Türchenfüllen";
+	public final static String ITEM_NAME = "Tag %s";
+	public final static String ITEM_EDIT_DAY = "Türchen %s";
+	public final static String ITEM_OPENED = "Bereits geöffnet";
+	public final static String ITEM_CLOSED = "Klicke zum Öffnen";
+	public final static String ITEM_FUTURE = "Überraschung in %s Tag%s";
+	public final static String ITEM_24 = "Weihnachten in %s Tag%s";
+	public final static String ITEM_24_READY = "Frohe Weihnachten!";
+	public final static String MSG_OPEN = "Du hast das Türchen Nummer %d bereits geöffnet!";
+	public final static String MSG_CLOSED = "Du darfst das Türchen Nummer %d noch nicht öffnen!";
+	public final static String MSG_OPENING = "Du hast Türchen Nummer %d geöffnet!";
+	public final static String MSG_OVERFLOW = "Dein Inventar ist voll. Der Türcheninhalt hat sich um dich herum verteilt.";
+	public final static String MSG_MISSING_PERM = "Du darfst das leider nicht tun! (Versuche es nochmal in der Hauptwelt)";
+	public final static String MSG_MISSING_CONTENT = "Die faulen Admins haben das Türchen noch nicht gefüllt ;)";
+	public final static String MSG_WRONG_TIME = "Du kannst den Adventskalender nicht mehr öffnen.";
+	public final static String MSG_WRONG_GAMEMODE = "Du solltest die Türchen nur im Survival-Modus öffnen.";
+	public final static String MSG_UNOPENED_DOORS = "Du hast noch ungeöffnete Kalendertürchen.";
+	public final static String MSG_COMMAND_INFO = "Du kannst den Adventskalender mit /advent öffnen.";
+
+	private static BaseComponent prefixedMessage(BaseComponent message) {
+		BaseComponent prefixedMessage = new TextComponent(PREFIX);
+		prefixedMessage.addExtra(message);
+		return prefixedMessage;
+	}
+
+	public static void send(CommandSender receiver, String templateMessage, Object... values) {
+		send(receiver, new TextComponent(String.format(templateMessage, values)));
+	}
+
+	public static void send(CommandSender receiver, BaseComponent message) {
+		receiver.spigot().sendMessage(prefixedMessage(message));
+	}
+
+	public static void notifyAboutUnopenedDoors(CommandSender receiver) {
+		send(receiver, new TextComponent(MSG_UNOPENED_DOORS + "\n" + MSG_COMMAND_INFO));
+	}
 }
