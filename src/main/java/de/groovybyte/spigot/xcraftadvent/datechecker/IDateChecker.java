9de2f05733101279eb59371f894b60f8a17684cc
Maximilian Stiede <maximilian.stiede@outlook.com>
2024-12-02 08:06:28 +0100

    tmp

---
+++ src/main/java/de/groovybyte/spigot/xcraftadvent/datechecker/IDateChecker.java
@@ -0,0 +1,12 @@
+package de.groovybyte.spigot.xcraftadvent;
+
+public interface IDateChecker {
+
+    int getDayOfMonth();
+
+    boolean isCalendarTime();
+    
+    boolean canCreateNewCalendar();
+
+    boolean canOpenExistingCalendar();
+}
